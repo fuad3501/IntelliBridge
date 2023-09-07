585359d24478c64e4c4af25604fb614597485d2c
fuad3501 <fuad3501@outlook.com>
2023-09-07 21:55:12 +0100

    setup tailwind

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    
+    return (
+        <div>
+            <p1>NavBar</p1>
+        </div>
+      );
+
+    }
+
+export default NavBar;
---
+++ src/pages/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+  <body class="body-page">
+
+  </body>
+  )
+}
+
+export default App
